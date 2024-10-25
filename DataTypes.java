1857cabcf3600869021ccc1c86586ca05c5b6cb4
Chithra-sundar <chithrasundar2001@gmail.com>
2024-10-25 06:14:35 +0530

    Added about DataTypes

---
+++ DataTypes.java
@@ -0,0 +1,11 @@
+public class DataTypes{
+    public static void main(String[] args) {
+        boolean flag = true;
+        byte bin = -116;
+        char zero = 48;
+        short small_number = 1000;
+        int number = 250000;
+        float pi = 3.14f;
+        System.out.println(pi);
+    }
+}
